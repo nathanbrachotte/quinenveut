15431a3be6999f98bd25ac32278d950b7df6d9b2
a-sellier <sellier.aurelie@gmail.com>
2017-10-07 20:17:28 +0200

    Ajout de la page objets en vente

---
+++ application/controllers/Objets.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a-sellier
+ * Date: 07/10/17
+ * Time: 20:08
+ */
+
+class Objets extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('objets_model');
+    }
+
+    public function lister()
+    {
+        $data['title'] = 'Liste des objets';
+
+        $data['objets'] = $this->objets_model->get_objets();
+
+        $this->load->view('templates/header',$data);
+        $this->load->view('liste_objets', $data);
+        $this->load->view('templates/footer');
+    }
+}
--- application/views/liste_objets.php
+++ application/views/liste_objets.php
@@ -1,1 +1,27 @@
-<!-- page listant toutes les objets en vente -->
+<!--
+* Created by PhpStorm.
+* User: a-sellier
+* Date: 07/10/17
+* Time: 18:36
+-->
+<h1>Objets mis en vente</h1>
+
+<?php
+
+if (sizeof($objets) == 0)
+{
+    echo '<p>Aucun objet en vente !</p>';
+}
+
+echo '<ul>';
+foreach ($objets as $row)
+{
+
+    echo '<li>'.$row['nom_objet'].' : '.$row['desc_objet'].'</li>';
+}
+
+echo '</ul>';
+
+
+?>
+
