ef562cb3257f7688f8b4285aaa28f4cfceb44a0b
a-sellier <sellier.aurelie@gmail.com>
2017-10-07 22:37:23 +0200

    Ajout de bootstrap

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -6,6 +6,10 @@
  *
 -->
 
-<em>&copy; 2017</em>
+        <em>&copy; 2017</em>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
