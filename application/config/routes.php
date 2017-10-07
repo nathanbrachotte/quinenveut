b4e395a1ad0726fb759b4f5eb4d5b3cb7712ff11
a-sellier <sellier.aurelie@gmail.com>
2017-10-07 22:36:34 +0200

    Ajout d'une route pour l'accueil

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'accueil/index';
 $route['404_override'] = 'errors/page_missing';
 $route['translate_uri_dashes'] = FALSE;
+
+
+$route['accueil'] = ['accueil/index'];
